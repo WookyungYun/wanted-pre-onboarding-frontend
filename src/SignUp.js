0f6aed7c68cdcd759ec18d5cafe071c59b13e998
WookyungYun <gyong1540@gmail.com>
2023-02-12 18:58:42 +0900

    feat: 회원가입 유효성 체크

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,17 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isEmail, setIsEmail] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
+    if (e.target.value.includes("@")) {
+      setIsEmail(true);
+    }
   };
   const handlePw = (e) => {
     setPassword(e.target.value);
+    if (e.target.value.length >= 8) {
+      setIsPassword(true);
+    }
   };
 
   const handleSignUpButton = () => {};
+
+  const handleButton = () => {
+    if (isEmail && isPassword) {
+      return false;
+    } else return true;
+  };
+
+  useEffect(() => {
+    //값 초기화
+    setIsEmail(false);
+    setIsPassword(false);
+  }, []);
 
   return (
     <>
@@ -20,11 +40,28 @@ function SignUp() {
           <p>회원가입</p>
         </header>
         Email
-        <input type="email" value={email} onChange={handleEmail} />
+        <input
+          type="text"
+          data-testid="email-input"
+          autoComplete="off"
+          value={email}
+          onChange={handleEmail}
+        />
         password
-        <input type="password" value={password} onChange={handlePw} />
+        <input
+          type="text"
+          data-testid="password-input"
+          autoComplete="off"
+          value={password}
+          onChange={handlePw}
+        />
       </div>
-      <button type="submit" onClick={handleSignUpButton} disabled="true">
+      <button
+        type="submit"
+        data-testid="signup-button"
+        onClick={handleSignUpButton}
+        disabled={handleButton()}
+      >
         회원가입
       </button>
     </>
